90c0df060855ecce845656b96c3c1c623c6130bb
Rustatano <jakub-svoboda111@seznam.cz>
2023-10-12 20:49:05 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::{fs::{self}, path::PathBuf};
+
+fn main() {
+    dig_deep(fs::read_dir("C:/").unwrap().map(|dir| dir.unwrap().path()).collect(), "windows");
+}
+
+fn dig_deep(dirs: Vec<PathBuf>, target: &str) {
+    for dir in &dirs {
+        if dir.is_file() {
+            match fs::read_to_string(dir) {
+                Ok(file) => read_file(file, target, dir),
+                Err(e) => eprintln!("ERROR: {}", e),
+            };
+
+        } else if dir == &PathBuf::from("C:/Windows") { // only on windows
+            eprintln!("WINDOWS FILES FOUND");
+        } else {
+            match fs::read_dir(dir) {
+                Ok(parent) => dig_deep(parent.map(|dir| dir.unwrap().path()).collect(), target),
+                Err(e) => eprintln!("ERROR: {}", e),
+            };
+        }
+    }
+}
+
+fn read_file(file: String, target: &str, dir: &PathBuf) {
+    let mut line_count = 0;
+    for line in file.lines() {
+        line_count += 1;
+        if line.contains(target) {
+            println!("{} found in {:?}, line {}", target, dir, line_count);
+        }
+    }
+}
