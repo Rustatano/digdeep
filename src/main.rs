e84cbe146d6f7307ec2bd22307d55587fe252f29
Rustatano <jakub-svoboda111@seznam.cz>
2023-10-12 22:14:16 +0200

    multithreading research

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::{fs::{self}, path::PathBuf};
 
 fn main() {
-    dig_deep(fs::read_dir("C:/").unwrap().map(|dir| dir.unwrap().path()).collect(), "windows");
+    dig_deep(fs::read_dir("../disksearch").unwrap().map(|dir| dir.unwrap().path()).collect(), "dig_deep");
 }
 
 fn dig_deep(dirs: Vec<PathBuf>, target: &str) {
     for dir in &dirs {
         if dir.is_file() {
+            if let Some(trg) = dir.file_name() {
+                if trg == target {
+                    println!("{:?} found in {:?}", target, dir);
+                }
+            }
+            
             match fs::read_to_string(dir) {
-                Ok(file) => {let mut line_count = 0;
+                Ok(file) => {
+                    let mut line_count = 0;
                     for line in file.lines() {
                         line_count += 1;
                         if line.contains(target) {
@@ -18,14 +25,17 @@ fn dig_deep(dirs: Vec<PathBuf>, target: &str) {
                 },
                 Err(e) => eprintln!("ERROR: {}", e),
             };
-
         } else if dir == &PathBuf::from("C:/Windows") { // only on windows
             eprintln!("WINDOWS FILES FOUND");
         } else {
             match fs::read_dir(dir) {
-                Ok(parent) => dig_deep(parent.map(|dir| dir.unwrap().path()).collect(), target),
+                Ok(parent) => dig_deep(parent.map(|dir| dir.unwrap().path()).collect(), target),// spawn_thread(parent, target),
                 Err(e) => eprintln!("ERROR: {}", e),
             };
         }
     }
 }
+/*
+fn spawn_thread(parent: ReadDir, target: &str) {
+    thread::spawn(|| dig_deep(parent.map(|dir| dir.unwrap().path()).collect(), target));
+} */
