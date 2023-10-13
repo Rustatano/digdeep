57afcc5854a81baa4683e4532792088cfb7b4085
Rustatano <jakub-svoboda111@seznam.cz>
2023-10-13 19:27:12 +0200

    no progress

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,49 @@
 use std::{fs::{self}, path::PathBuf, time::Instant};
 
+pub const ROOT: &str = "..\\dig_deep";
+pub const TARGET: &str = "main.rs";
+pub const SEARCH_IN_FILES: bool = false;
+
 fn main() {
     let now = Instant::now();
-    dig_deep(fs::read_dir("../disksearch").unwrap().map(|dir| dir.unwrap().path()).collect(), "dig_deep");
+    match fs::read_dir(ROOT) {
+        Ok(root) => dig_deep(root.map(|dir| dir.unwrap().path()).collect()),
+        Err(_) => eprintln!("DIRECTORY NOT FOUND"),
+    };
     println!("dig_deep done in {:?}", now.elapsed());
 }
 
-fn dig_deep(dirs: Vec<PathBuf>, target: &str) {
+fn dig_deep(dirs: Vec<PathBuf>) {
     for dir in &dirs {
         if dir.is_file() {
             if let Some(trg) = dir.file_name() {
-                if trg == target {
-                    println!("{:?} found in {:?}", target, dir);
+                if trg == TARGET {
+                    println!("{:?} found in {:?}", TARGET, dir);
                 }
             }
-            
-            match fs::read_to_string(dir) {
-                Ok(file) => {
-                    let mut line_count = 0;
-                    for line in file.lines() {
-                        line_count += 1;
-                        if line.contains(target) {
-                            println!("{} found in {:?}, line {}", target, dir, line_count);
+            if SEARCH_IN_FILES {
+                match fs::read_to_string(dir) {
+                    Ok(file) => {
+                        let mut line_count = 0;
+                        for line in file.lines() {
+                            line_count += 1;
+                            if line.contains(TARGET) {
+                                println!("{} found in {:?}, line {}", TARGET, dir, line_count);
+                            }
                         }
-                    }
-                },
-                Err(e) => eprintln!("ERROR: {}", e),
-            };
+                    },
+                    Err(e) => eprintln!("ERROR: {}", e),
+                }
+            }
 
         } else if dir == &PathBuf::from("C:/Windows") { // only on windows
             eprintln!("WINDOWS FILES FOUND");
 
         } else {
             match fs::read_dir(dir) {
-                Ok(parent) => dig_deep(parent.map(|dir| dir.unwrap().path()).collect(), target),// spawn_thread(parent, target),
+                Ok(parent) => dig_deep(parent.map(|dir| dir.unwrap().path()).collect()),
                 Err(e) => eprintln!("ERROR: {}", e),
-            };
+            }; 
         }
     }
 }
-
-/*
-fn spawn_thread(parent: ReadDir, target: &str) {
-    thread::spawn(|| dig_deep(parent.map(|dir| dir.unwrap().path()).collect(), target));
-} */
